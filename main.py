eb357ae7ff986839284bd2152ecda151e0564afe
BoGdysor <capetcarpet@gmail.com>
2021-10-20 13:35:15 +0300

    save_2

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-
-
-
-
-
 def read_list():
     """
     Citeste un string pe care il transforma intr-o lista de numere
@@ -16,6 +11,49 @@ def read_list():
     return int_list
 
 
+def even_length(list_1, list_2):
+    """
+    Verifica daca cele doua liste au acelasi numar de elemente pare
+    :param list_1: Lista cu numere intregi
+    :param list_2: Lista cu numere intregi
+    :return: 1 - Daca au acelasi numar de elemente pare, 0 - Daca nu au acelasi numar de elemente pare
+    """
+    even_count_1 = 0
+    even_count_2 = 0
+    for i in range(len(list_1)):
+        if list_1[i] % 2 == 0:
+            even_count_1 = even_count_1 + 1
+    for i in range(len(list_2)):
+        if list_2[i] % 2 == 0:
+            even_count_2 = even_count_2 + 1
+    if even_count_2 == even_count_1:
+        return 1
+    else:
+        return 0
+
+
+def test_even_length():
+    assert even_length([10, 20, 30, 40], [2, 4, 6, 8]) == 1
+    assert even_length([10, 2, 35, 41], [200, 400, 300, 500]) == 0
+    assert even_length([1, 11, 111, 111], [2, 4, 6, 8]) == 0
+
+def intersection(list_1,list_2):
+    """
+    
+    :param list_1: 
+    :param list_2: 
+    :return: 
+    """
+    intersected_list = []
+    for i in range(len(list_1)):
+        for j in range(len(list_2)):
+            if list_1[i] == list_2[j]:
+                if list_1[i] not in intersected_list:
+                    intersected_list.append(list_1[i])
+    return intersected_list
+
+
+
 def main():
     list_A = []
     list_B = []
@@ -27,14 +65,24 @@ def main():
             print("Introucrti a 2a lista: ")
             list_B = read_list()
         elif optiune == '2':
-            pass
+            op = even_length(list_A, list_B)
+            if op == 1:
+                print("Listele au acelasi numar de elemente pare")
+            else:
+                print("Listele nu au acelasi numar de elemente pare")
         elif optiune == '3':
-            pass
+            inter_list = intersection(list_A,list_B)
+            print(inter_list)
         elif optiune == '4':
             pass
         elif optiune == '5':
             pass
+        elif optiune == 'x':
+            break
+        else:
+            print("Optiune invalida!")
 
 
 if __name__ == '__main__':
+    test_even_length()
     main()
